b2bc593bdd78eee8bf77c65a62896a0e94ae85de
Nicholas Nobile <nickcnobile@gmail.com>
2020-06-20 16:45:53 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,3 +1,5 @@
-def say_hello(name = "Kent Beck" "Ruby Programmer")
+def say_hello(name = "Kent Beck")
   puts  "Hello #{name}!"
+def greeting(name = "Ruby Programmer")
+  puts "Hello, #{name}!
 end
